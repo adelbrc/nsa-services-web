0137e96b0d043ad635af1fdbb7c4f2ebfdb3bc2a
samysab <samy.saberi@gmail.com>
2020-02-10 14:47:53 +0100

    fix : Header client

--- libs/php/includes/userHeader.php
+++ libs/php/includes/userHeader.php
@@ -12,7 +12,7 @@
         <a class="nav-link" href="../index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="../dashboard.php">User Panel</a>
+        <a class="nav-link" href="../dashboard.php">Planning</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
