4161a58e4d255b9c412361282d28fa8c00e28b81
samysab <samy.saberi@gmail.com>
2020-02-10 11:39:54 +0100

    Front : Modal Btn reservation

--- dashboard.php
+++ dashboard.php
@@ -6,41 +6,41 @@ if (!isConnected()) {
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>dashboard</title>
-    <link rel="stylesheet" type="text/css" href="ressources/style/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="ressources/style/user.css">
+<head>
+  <meta charset="utf-8">
+  <title>dashboard</title>
+  <link rel="stylesheet" type="text/css" href="ressources/style/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="ressources/style/user.css">
 
 
-  </head>
-  <body>
-    <?php include('libs/php/includes/userHeader.php');
-    include('libs/php/db/db_connect.php');
-    ?>
-    <div>
-      <h2 style="text-align: center; font-size: 50px; padding-top: 50px">Découvrez nos services</h2>
-    </div>
-    <hr class="my-4">
+</head>
+<body>
+  <?php include('libs/php/includes/userHeader.php');
+  include('libs/php/db/db_connect.php');
+  ?>
+  <div>
+    <h2 style="text-align: center; font-size: 50px; padding-top: 50px">Découvrez nos services</h2>
+  </div>
+  <hr class="my-4">
 
-    <div id="role"class="scrollmenu">
-      <?php
-      $q = $conn->query('SELECT * FROM role');
-      while($result = $q->fetch()){
-       ?>
+  <div id="role"class="scrollmenu">
+    <?php
+    $q = $conn->query('SELECT * FROM role');
+    while($result = $q->fetch()){
+      ?>
       <div class="col-md-3 mx-auto">
         <div class="jumbotron">
           <h1 class="display-5" style="text-align: center;"><?php echo $result['name']?></h1>
           <hr class="my-4">
           <div id="btnInteresser" class="mx-auto">
-            <a style="margin-left: 30%;" type="button" class="btn btn-info" href="#">Réserver</a>
+            <?php include('libs/php/includes/newReservationBtn.php'); ?>
           </div>
         </div>
       </div>
     <?php } ?>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ libs/php/includes/newReservationBtn.php
@@ -0,0 +1,38 @@
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModal" style="margin-left: 30%;">
+  Réserver
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"><?php echo $result['name']; ?></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form class="formSub" action="newsubscription.php" method="post">
+          <div class="form-group">
+            <label for="exampleInputEmail1">Nombre d'heure</label>
+            <input type="number" class="form-control" id="exampleInputnbHours" aria-describedby="nbHoursHelp" placeholder="Nombre d'heure" name="nbHours" value="<?php if(isset($nbHours)) { echo $nbHours; } ?>">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Date de réservation</label>
+            <input type="date" class="form-control" id="exampleInputdateResa" aria-describedby="dateResaHelp" placeholder="date" name="dateResa" value="<?php if(isset($dateResa)) { echo $dateResa; } ?>">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Description</label>
+            <input type="text" style="height: 100px;" class="form-control" id="exampleInputDescription" aria-describedby="CloseDescription" name="Description" value="<?php if(isset($Description)) { echo $Description; } ?>">
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" name="formNewSub" class="btn btn-primary">Confirmer la réservation</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
