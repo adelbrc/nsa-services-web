cc398913b6060aa3b6e6d4ad5abd10cc29ae709a
samysab <samy.saberi@gmail.com>
2020-02-10 10:27:48 +0100

    Back office : href customer dans sidebar

--- admin/displayUsers.php
+++ admin/displayUsers.php
@@ -16,19 +16,20 @@ if(isset($_GET['id']) AND !empty($_GET['id'])) {
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Gestion des abonnements</title>
-    <link rel="stylesheet" type="text/css" href="../ressources/style/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../ressources/style/dashHeader.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../ressources/style/admin.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="../ressources/style/admin.css">
-
 
   </head>
   <body>
-    <?php include('../libs/php/mainHeader.php'); ?>
+    <header>
+    <?php include('includes/headerNavigation.php'); ?>
+  </header>
     <div class="container-fluid">
       <div class="row">
-        <?php include('dashHeader.php');?>
+        <?php include('includes/sidebar.php'); ?>
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
           <div class=" border jumbotron col-md-12" id="jumboRole" style="margin-top: 75px;">
             <h1 class="display-5" style="text-align: center;">Utilisateurs</h1>
--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -20,7 +20,7 @@
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">
+        <a class="nav-link" href="displayUsers.php">
           <span data-feather="users"></span>
           Customers
         </a>
