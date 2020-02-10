e16ab0aa8f57320c6541d69156b1a8c79ed96561
adelbrc <adelbouaricha@protonmail.com>
2020-02-10 23:11:04 +0100

    Fix : Nom de page dans le header

--- libs/php/includes/userHeader.php
+++ libs/php/includes/userHeader.php
@@ -12,7 +12,7 @@
         <a class="nav-link" href="../index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="../dashboard.php">Planning</a>
+        <a class="nav-link" href="../dashboard.php">Dashboard</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
