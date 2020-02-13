88c30d027cf7b13a653cfeaa4ebd6ac76f03143d
samysab <samy.saberi@gmail.com>
2020-02-13 10:19:09 +0100

    Back Office : Creation des role et service - Debut

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -25,6 +25,12 @@
           Partners
         </a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="services_management.php">
+          <span data-feather="users"></span>
+          Services
+        </a>
+      </li>
     </ul>
 
     <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1">
