d9a70ee1799655587f01365a816b8463c394ffdf
samysab <samy.saberi@gmail.com>
2020-02-13 09:53:07 +0100

    Front Office : Ajout d'un carousel dans l'Index

--- index.php
+++ index.php
@@ -19,9 +19,44 @@ include('libs/php/db/db_connect.php');?>
 				Vous n'avez pas le droit d'accéder à cette page.
 			</div>
 		<?php } ?>
-		<div id="video">
-					VIDEO
-				</div>
+		<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+		  <ol class="carousel-indicators">
+		    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+		    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+		    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+		  </ol>
+		  <div class="carousel-inner">
+		    <div class="carousel-item active">
+		      <img src="ressources/img/premium-individuel-800x513.jpg" style="height: 920px;"  class="d-block w-100">
+		      <div class="carousel-caption d-none d-md-block">
+		        <h5>Un service à l'écoute </h5>
+		        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+		      </div>
+		    </div>
+		    <div class="carousel-item">
+		      <img src="ressources/img/nsa-services.png" style="height: 920px;" class="d-block w-100" alt="">
+		      <div class="carousel-caption d-none d-md-block">
+		        <h5>Second slide label</h5>
+		        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+		      </div>
+		    </div>
+		    <div class="carousel-item">
+		      <img src="ressources/img/premium-individuel-800x513.jpg" style="height: 920px;" class="d-block w-100" >
+		      <div class="carousel-caption d-none d-md-block">
+		        <h5>Third slide label</h5>
+		        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+		      </div>
+		    </div>
+		  </div>
+		  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
 				<div id="sommeNous">
 					<div class="col-md-6 quiSommeNous">
 						<img src="ressources/img/premium-individuel-800x513.jpg" alt="">
