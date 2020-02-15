52731c55ca58424314ea99756b3cf7d0895297ba
adelbrc <adelbouaricha@protonmail.com>
2020-02-15 03:32:47 +0100

    Fix: Responsive Tables Back Office

--- libs/php/views/ordersList.php
+++ libs/php/views/ordersList.php
@@ -5,42 +5,44 @@ $list = Order::getAllOrders();
 <div class="dataContainer">
   <h3 class="text-center">Orders List</h3>
   <div class="row">
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Customer ID</th>
-          <th scope="col">Order Date</th>
-          <th scope="col">Quantity</th>
-          <th scope="col">Service ID</th>
-          <th scope="col">Payment Status</th>
-          <th scope="col">Reservation Date</th>
-          <th scope="col">Order Status</th>
-          <th scope="col">Cancel</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach ($list as $key => $order): ?>
+    <div class="table-responsive">
+      <table class="table">
+        <thead>
           <tr>
-            <th scope="row"><?php echo $order->getOrderId(); ?></th>
-            <td><?php echo $order->getCustomerId(); ?></td>
-            <td><?php echo $order->getOrderDate(); ?></td>
-            <td><?php echo $order->getNbHours(); ?></td>
-            <td><?php echo $order->getServiceId(); ?></td>
-            <td><?php echo $order->getPaymentStatus(); ?></td>
-            <td><?php echo $order->getReservationDate(); ?></td>
-            <td><?php echo $order->getOrderStatus(); ?></td>
-            <td>
-              <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalCancel<?php echo $order->getOrderId(); ?>">
-                <i class="fa fa-close"></i>
-              </button>
-            </td>
+            <th scope="col">ID</th>
+            <th scope="col">Customer ID</th>
+            <th scope="col">Order Date</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">Service ID</th>
+            <th scope="col">Payment Status</th>
+            <th scope="col">Reservation Date</th>
+            <th scope="col">Order Status</th>
+            <th scope="col">Cancel</th>
           </tr>
-          <!-- Modale Delete Partner -->
-          <?php include("../libs/php/includes/cancelOrderModal.php"); ?>
-          <!-- End Modal -->
-        <?php endforeach; ?>
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          <?php foreach ($list as $key => $order): ?>
+            <tr>
+              <th scope="row"><?php echo $order->getOrderId(); ?></th>
+              <td><?php echo $order->getCustomerId(); ?></td>
+              <td><?php echo $order->getOrderDate(); ?></td>
+              <td><?php echo $order->getNbHours(); ?></td>
+              <td><?php echo $order->getServiceId(); ?></td>
+              <td><?php echo $order->getPaymentStatus(); ?></td>
+              <td><?php echo $order->getReservationDate(); ?></td>
+              <td><?php echo $order->getOrderStatus(); ?></td>
+              <td>
+                <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalCancel<?php echo $order->getOrderId(); ?>">
+                  <i class="fa fa-close"></i>
+                </button>
+              </td>
+            </tr>
+            <!-- Modale Delete Partner -->
+            <?php include("../libs/php/includes/cancelOrderModal.php"); ?>
+            <!-- End Modal -->
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    </div>
   </div>
 </div>
--- libs/php/views/partnersList.php
+++ libs/php/views/partnersList.php
@@ -5,54 +5,56 @@ $list = Partner::getAllPartners();
 <div class="dataContainer">
   <h3 class="text-center">Partners List</h3>
   <div class="row">
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Corporation</th>
-          <th scope="col">SIREN</th>
-          <th scope="col">Firstname</th>
-          <th scope="col">Lastname</th>
-          <th scope="col">Email</th>
-          <th scope="col">Phone</th>
-          <th scope="col">City</th>
-          <th scope="col">Edit</th>
-          <th scope="col">Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach ($list as $key => $partner): ?>
+    <div class="table-responsive">
+      <table class="table">
+        <thead>
           <tr>
-            <th scope="row"><?php echo $partner->getPID(); ?></th>
-            <td><?php echo $partner->getCorpName(); ?></td>
-            <td><?php echo $partner->getCorpId(); ?></td>
-            <td><?php echo $partner->getFirstname(); ?></td>
-            <td><?php echo $partner->getLastname(); ?></td>
-            <td><?php echo $partner->getEmail(); ?></td>
-            <td><?php echo $partner->getPhoneNumber(); ?></td>
-            <td><?php echo $partner->getCity(); ?></td>
-            <td>
-              <!-- Button trigger modal -->
-              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalUpdate<?php echo $partner->getPID(); ?>">
-                <i class="fa fa-cog"></i>
-              </button>
-            </td>
-            <td>
-              <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalDelete<?php echo $partner->getPID(); ?>">
-                <i class="fa fa-close"></i>
-              </button>
-            </td>
+            <th scope="col">#</th>
+            <th scope="col">Corporation</th>
+            <th scope="col">SIREN</th>
+            <th scope="col">Firstname</th>
+            <th scope="col">Lastname</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">City</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
           </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($list as $key => $partner): ?>
+            <tr>
+              <th scope="row"><?php echo $partner->getPID(); ?></th>
+              <td><?php echo $partner->getCorpName(); ?></td>
+              <td><?php echo $partner->getCorpId(); ?></td>
+              <td><?php echo $partner->getFirstname(); ?></td>
+              <td><?php echo $partner->getLastname(); ?></td>
+              <td><?php echo $partner->getEmail(); ?></td>
+              <td><?php echo $partner->getPhoneNumber(); ?></td>
+              <td><?php echo $partner->getCity(); ?></td>
+              <td>
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalUpdate<?php echo $partner->getPID(); ?>">
+                  <i class="fa fa-cog"></i>
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalDelete<?php echo $partner->getPID(); ?>">
+                  <i class="fa fa-close"></i>
+                </button>
+              </td>
+            </tr>
 
-          <!-- Modal Update Partner -->
-          <?php include("../libs/php/includes/updatePartnerModal.php"); ?>
-          <!-- End Modal -->
+            <!-- Modal Update Partner -->
+            <?php include("../libs/php/includes/updatePartnerModal.php"); ?>
+            <!-- End Modal -->
 
-          <!-- Modale Delete Partner -->
-          <?php include("../libs/php/includes/deletePartnerModal.php"); ?>
-          <!-- End Modal -->
-        <?php endforeach; ?>
-      </tbody>
-    </table>
+            <!-- Modale Delete Partner -->
+            <?php include("../libs/php/includes/deletePartnerModal.php"); ?>
+            <!-- End Modal -->
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    </div>
   </div>
 </div>
