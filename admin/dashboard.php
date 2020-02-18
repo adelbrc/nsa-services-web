34236e58e3bc703d360ad90dcf483813803834e6
adelbrc <adelbouaricha@protonmail.com>
2020-02-18 17:33:18 +0100

    Fix: Suppression des fichiers Chart.js

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -51,44 +51,26 @@ if ($_SESSION['user']['rank'] !=3) {
       </div>
     </div>
 
-    <script src="../libs/js/node_modules/chart.js/dist/Chart.js" charset="utf-8"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     <script>
     var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+                label: 'My First dataset',
+                backgroundColor: 'rgba(79, 176, 246, 1)',
+                borderColor: 'rgb(46, 129, 182)',
+                data: [0, 10, 5, 2, 20, 30, 45]
             }]
         },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
+
+        // Configuration options go here
+        options: {}
     });
     </script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
