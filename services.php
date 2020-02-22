a691fdd8704c6f99b40bd993eceab3d92c0b8918
adelbrc <adelbouaricha@protonmail.com>
2020-02-22 16:22:24 +0100

    Commande Service Basique Stripe

--- services.php
+++ services.php
@@ -2,7 +2,11 @@
 
 require_once("libs/php/classes/User.php");
 require_once("libs/php/classes/Service.php");
+require_once('libs/stripe-php-master/init.php');
 include("libs/php/isConnected.php");
+
+// Init Stripe API KEY
+\Stripe\Stripe::setApiKey('sk_test_UDEhJY5WRNQMQUmjcA20BPne00XeEQBuUc');
 
 ?>
 <!DOCTYPE html>
@@ -38,5 +42,39 @@ include("libs/php/isConnected.php");
         </div>
       </section>
     </main>
+    <script src="https://js.stripe.com/v3/"></script>
+    <script>
+
+
+      var PUBLISHABLE_KEY = "pk_test_ez95S8pacKWv7L234McLkmLE00qanCpC2B";
+
+      var DOMAIN = http://nsaservices.local;
+
+      var stripe = Stripe(PUBLISHABLE_KEY);
+
+      // Handle any errors from Checkout
+      var handleResult = function(result) {
+        if (result.error) {
+          var displayError = document.getElementById("error-message");
+          displayError.textContent = result.error.message;
+        }
+      };
+
+      // cette fonction permet de rediriger vers le site de stripe avec l'id du plan
+      var redirectToCheckout = function(id_product) {
+        stripe
+          .redirectToCheckout({
+            items: [{ product: id_product, quantity: 1 }],
+            successUrl:
+              // "https://" +
+              DOMAIN +
+              "/pages_stripe/success.html?session_id={CHECKOUT_SESSION_ID}",
+            // cancelUrl: "https://" + DOMAIN + "/canceled.html"
+            cancelUrl: DOMAIN + "/pages_stripe/canceled.html"
+          })
+          .then(handleResult);
+      };
+
+    </script>
   </body>
 </html>
