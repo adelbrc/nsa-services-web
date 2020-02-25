150bb7f4cf680e0e624201e9f90ebc86324df274
samysab <samy.saberi@gmail.com>
2020-02-25 11:56:24 +0100

    Feature : Creation des service dans Stripe

--- libs/php/controllers/newService.php
+++ libs/php/controllers/newService.php
@@ -7,6 +7,32 @@
     $category = htmlspecialchars($_POST['category']);
     $description = htmlspecialchars($_POST['inputDescription']);
 
+
+    // on va l'inserer (1) dans stripe puis (2) en bdd
+    // comme ca on peut recuperer le id_plan genere par stripe pour le mettre en bdd
+
+    // (1) Insert dans stripe
+    require_once('../../stripe-php-master/init.php');
+    \Stripe\Stripe::setApiKey('sk_test_UDEhJY5WRNQMQUmjcA20BPne00XeEQBuUc');
+
+    $newService = \Stripe\Product::create([
+      'type' => 'good',
+      'name' => $name
+    ]);
+
+
+    $sku1 = \Stripe\SKU::create([
+    'currency' => 'usd',
+    'inventory' => [
+        'type' => 'finite',
+        'quantity' => 500,
+    ],
+    'price' => $price,
+    'product' => $newService["id"]
+    ]);
+    echo $newService['id'];
+    echo $price;
+
     include('../db/db_connect.php');
     $insertRole = $conn->prepare("INSERT INTO service(name, price, discountPrice, category_id, description ) VALUES(?, ?, ?, ?, ?)");
 
