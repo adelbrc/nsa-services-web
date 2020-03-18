303afa0c8da17c11299470b9bf68dc2c5fdae86d
samysab <samy.saberi@gmail.com>
2020-03-18 16:13:59 +0100

    Feature -Front : Traduction Anglais/Francais

---
+++ libs/php/functions/translation.php
@@ -0,0 +1,37 @@
+<?php
+// INDEX.PHP
+$aLecoute = array("Un service à l'écoute", "A service that listens");
+$quiSommeNous = array("Qui sommes nous ?", "Who are we?");
+$quiSommeNoustexte = array("NSA Services est un service de conciergerie privée à destination d’une clientèle haut de gamme, exigeante, pressée, à qui nous offrons l’excellence, la perfection et le sur-mesure.
+", "NSA Services is a private concierge service for an upscale, demanding, hurried clientele to whom we offer excellence, perfection and customization.");
+$aboIndiv = array("Abonnement individuels", "Individual Subscriptions");
+$open = array("Ouvert", "Open");
+$from = array("de", "from");
+$to = array("a", "to");
+
+//SIGNUP.PHP
+$inscription = array("Inscription", "Registration");
+$passerror = array("Vos mot de passe ne correspondent pas !", "Your passwords don't match!");
+$pseudolength = array("Votre pseudo ne peut pas dépasser 15 caracteres !", "Your nickname cannot exceed 15 characters!");
+$lastnameLength = array("Votre nom ne peut pas dépasser 15 caracteres !", "Your name cannot be longer than 15 characters!");
+$field_blank = array("Tout les champs doivent être compléter !", "All fields must be completed!");
+$name_length = array("Votre prénom ne peut pas dépasser 15 caracteres !", "Your first name cannot exceed 15 characters!");
+$invalid_email = array("Votre adresse mail n\'est pas valide !", "Your email address is not valid!");
+$email_exist = array("Email déja existant", "Email already existing");
+$Nom = array('Nom', 'Lastame');
+$Prenom = array('Prenom', 'Firstname');
+$address = array('Adresse Postal', 'Postal Address');
+$Ville = array('Ville', 'City');
+$telNumber = array('Numéro de telephone', 'Phone number');
+$passwd = array('Mot de passe', 'Password');
+$confimPasswd = array('confirmer le mot de passe', 'Confirm your Password');
+$seConnecter = array('Se connecter', 'Login');
+$saisir = array('Saisir', 'Enter');
+$reSaisir = array('Retappez', 'Re-type');
+
+//LOGIN.PHP
+$connexion = array('Connexion', 'Login');
+$wrongpass = array('Mauvais mot de passe' ,'Wrong password');
+$rediInscri = array('Vous pouvez désormais vous connecter', 'You can now log in');
+$pasEncoreInscrit = array('Pas encore inscrit ? Rejoingnez-nous !', 'Not registered yet? Join us!');
+ ?>
