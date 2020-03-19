561784a702ab7ea383e4f3f13f4019d0eb3d92b8
Narek <narek.khatchatrian@live.fr>
2020-03-19 01:28:42 +0100

    ajout planning static

--- mes_services.php
+++ mes_services.php
@@ -47,6 +47,106 @@ if (isset($_GET["session_id"]) && !empty($_GET["session_id"])) {
 		<link rel="stylesheet" href="ressources/style/style.css">
 
 		<title>Mes services - Home Services</title>
+
+
+		<link href='libs/fullcalendar/packages/core/main.css' rel='stylesheet' />
+		<link href='libs/fullcalendar/packages/daygrid/main.css' rel='stylesheet' />
+
+		<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.6.0/fullcalendar.css' />
+
+
+		<link href='libs/fullcalendar/packages/core/main.css' rel='stylesheet' />
+		<link href='libs/fullcalendar/packages/daygrid/main.css' rel='stylesheet' />
+		<link href='libs/fullcalendar/packages/timegrid/main.css' rel='stylesheet' />
+
+		<script src='libs/fullcalendar/packages/core/main.js'></script>
+		<script src='libs/fullcalendar/packages/daygrid/main.js'></script>
+		<script src='libs/fullcalendar/packages/timegrid/main.js'></script>
+
+		<!-- <link rel='stylesheet' href='themes/base/jquery-ui.min.css' /> -->
+
+		<link href='https://use.fontawesome.com/releases/v5.0.6/css/all.css' rel='stylesheet'>
+		<link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' rel='stylesheet' />
+
+
+		<script>
+
+			document.addEventListener('DOMContentLoaded', function() {
+				var calendarEl = document.getElementById('calendar');
+
+				var calendar = new FullCalendar.Calendar(calendarEl, {
+					columnHeaderHtml: function(date) {
+						// if (date.getUTCDay() === 5) {
+							var tab_jour = new Array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
+							return ""+tab_jour[date.getDay()]+"";
+					},
+
+					plugins: [  'dayGrid', 'timeGrid', 'bootstrap' ],
+					defaultView: 'timeGridWeek',
+
+					aspectRatio: 1.85,
+					// height: 600,
+					// contentHeight: 500,
+
+					theme: true,
+					
+					// header
+					header: {
+					  left: 'prev,next today',
+					  center: 'title',
+					  right: 'timeGridDay,timeGridWeek,dayGridMonth'
+					},
+
+					firstDay: 1,
+					
+					// cache mardi jeudi
+					// hiddenDays: [ 2, 4 ], 
+
+					locale: "fr",
+
+					// l'interval de temps sur la colonne de gauche
+					slotDuration:'00:30:00',
+
+					// l'heure du debut du planning
+					minTime: "09:00:00",
+
+					// l'heure de fin
+					maxTime: "21:00:00",
+
+					events: [
+						{
+							title:"Meeting1",
+							start:"2020-03-18T10:00:00+00:00",
+							end:"2020-03-18T11:00:00+00:00",
+							allDay : false // will make the time show
+						},
+
+						{
+							title:"Meeting2",
+							start:"2020-03-18T12:30:00+00:00",
+							end:"2020-03-18T14:30:00+00:00",
+							allDay : false // will make the time show
+						},
+
+						{
+							title:"Meeting3",
+							start:"2020-03-18T14:30:00+00:00",
+							end:"2020-03-18T16:30:00+00:00",
+							allDay : false // will make the time show
+						},
+
+					]
+				});
+
+				calendar.render();
+			});
+
+		</script>
+
+
+
+
+
 	</head>
 	<body>
 		<header>
@@ -74,6 +174,7 @@ if (isset($_GET["session_id"]) && !empty($_GET["session_id"])) {
 				<div class="dataContainer">
 					<h2 class="text-center">Services prévus</h2>
 
+						<div id='calendar'></div>
 
 
 <!-- 						<table class="table">
@@ -176,6 +277,15 @@ if (isset($_GET["session_id"]) && !empty($_GET["session_id"])) {
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.full.min.js"></script>
 
+
+		<script src='libs/fullcalendar/packages/timegrid/main.js'></script>
+		<script src='libs/fullcalendar/packages/daygrid/main.js'></script>
+		<script src='libs/fullcalendar/packages/core/main.js'></script>
+
+		<script src='libs/fullcalendar/packages/core/locales-all.js'></script>
+		<script src='http://code.jquery.com/jquery-1.11.3.min.js'></script>
+		<script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.1/moment.min.js'></script>
+		<script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.6.0/fullcalendar.min.js'></script>
 
 
 		<!-- JS -->
