4ec793f745554e9ad3ea6dd074dd5d86d6e364a5
samysab <samy.saberi@gmail.com>
2020-04-10 19:20:33 +0200

    Fix : partnerHeader

--- libs/php/includes/partnerHeader.php
+++ libs/php/includes/partnerHeader.php
@@ -24,16 +24,6 @@
 				<a class="dropdown-item" href="mon_planning.php">Interventions</a>
 			</div>
     </li>
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
       <li class="nav-item">
         <a class="nav-link" href="../libs/php/deconnexion.php">Logout</a>
       </li>
